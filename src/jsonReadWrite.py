6bfe30bace9d1ea0c64cffda4a1e7a2c74e6accc
Windi159 <107599426+Windi159@users.noreply.github.com>
2024-02-21 23:24:15 +0900

    Update jsonReadWrite.py

--- src/jsonReadWrite.py
+++ src/jsonReadWrite.py
@@ -1,14 +1,10 @@
 import json
-from os.path import isfile
+
 def readJson(user_name):
-    if isfile(f".\\userInfo\\{user_name}.json"):
-        with open(f".\\userInfo\\{user_name}.json", "r", encoding="utf-8") as file:
-            user_info_file = json.load(file)
+    with open(f".\\userInfo\\{user_name}.json", "r", encoding="utf-8") as file:
+        user_info_file = json.load(file)
 
-            return user_info_file
-
-    else:
-        return None
+        return user_info_file
 
 def writeJson(user_name, user_info):
     with open(f".\\userInfo\\{user_name}.json", "w", encoding="utf-8") as make_file:
